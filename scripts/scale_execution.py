f73e26d0f9d5e550816ce5824858b88a35f15b27
Lucca Bavia <lucca.bavia@MacBook-Air.local>
2020-07-04 02:00:39 -0300

    Organize script

--- scripts/scale_execution.py
+++ scripts/scale_execution.py
@@ -9,9 +9,16 @@ from string_matching import ParallelPreprocessing as pp
 from string_matching import ParallelStringMatching as psm
 import multiprocessing as mp
 from create_big_file import create_large_file
-# import pandas as pd
 
 def get_core_increment(max_core_count):
+    """
+    Create list with core counts used in scaling test (values are power of 2).
+
+    Parameters:
+    -----------
+    max_core_count: `int`
+        Max core count used
+    """
     
     i = 0
     core_count = []
@@ -23,10 +30,20 @@ def get_core_increment(max_core_count):
             break
         i += 1
     
-    print(core_count)
+    print("Core counts used in test -> {}".format(core_count))
     return core_count
     
 def log_data(file, result):
+    """
+    Log results from tests.
+
+    Parameters:
+    -----------
+    file: `str`
+        Path to file used for logging
+    result:
+        Data entry to be saved.
+    """
 
     with open(file, "a+") as f:
         f.write("\n")
@@ -34,120 +51,88 @@ def log_data(file, result):
             f.write(str(i)+"\n")
 
 def execute_algorithm(pattern, text, log_file,
-            max_cores=mp.cpu_count(), naive=True, exec_async=False, repeat=10):
-    print(len(text))
+            max_cores=mp.cpu_count(), naive=True, repeat=10):
+    """
+    Execute algorithm repeatedly, to test scaling.
+
+    Parameters:
+    -----------
+    pattern: `str`
+        Pattern to look for in text
+    text: `text`
+        Text used as haystack
+    log_file: `str`
+        Path to file used for logging data
+    max_cores: `int`
+        Max core count used for tests
+    naive: `bool`
+        Execute with Naive algorithm
+    repeat: `int`
+        Number of iterations used for test
+    """
+    
     sequentialSM = sm()
     parallelSM = psm()
     
     core_count_list = get_core_increment(max_cores)
     result_data = []
-    # core_count_list=[4]
-    if exec_async:
-        if naive:
-            
-            for cores in core_count_list:
-                print(f"\nStarting tests with {cores} cores")
-                sliced_text, segments = pp.get_text_segments(text, cores)
+        
+    if naive:
+        print("Using Naive search algorithm!")
+        for cores in core_count_list:
+            print("Starting tests with {} core(s)".format(cores))
+            sliced_text, segments = pp.get_text_segments(text, cores)
 
-                with mp.Pool(cores) as pool:
-                    for iteration in range(repeat): 
-                        start = time.time()
-                        results_pool = pool.starmap_async(
-                            parallelSM.naive_algorithm, 
-                            [(pattern, x, y) for x, y in zip(sliced_text, segments)]
-                        )
-                        end = time.time()
+            with mp.Pool(cores) as pool:
+                for iteration in range(repeat): 
+                    start = time.time()
+                    results_pool = pool.starmap_async(
+                        parallelSM.naive_algorithm, 
+                        [(pattern, x, y) for x, y in zip(sliced_text, segments)]
+                    )
+                    end = time.time()
 
-                        results = list()
-                        for i in results_pool.get():
-                            results = results + i
-                        result_data.append((cores, end - start, len(results)))
-                        # print("Naive parallel synced elapsed: ", end - start)
-                    time.sleep(1)
-            print(result_data)
-            log_data(log_file, result_data)
+                    results = list()
+                    for i in results_pool.get():
+                        results = results + i
+                    result_data.append((cores, end - start, len(results)))
+                    print("        Naive parallel elapsed: ", end - start)
+                time.sleep(1)
+        log_data(log_file, result_data)
 
-        else:
-            lps = sequentialSM.build_lps(pattern)
-
-            for cores in core_count_list:
-                print(f"Starting tests with {cores} core(s)")
-                sliced_text, segments = pp.get_text_segments(text, cores)
-
-                with mp.Pool(cores) as pool:
-                    for iteration in range(repeat):
-                        print(f"    Iteration {iteration}") 
-                        start = time.time()
-                        results_pool = pool.starmap_async(
-                            parallelSM.kmp_algorithm, 
-                            [(pattern, x, y) for x, y in zip(sliced_text, segments)]
-                        )
-                        results_pool = results_pool.get()
-                        end = time.time()
-
-                        results = list()
-                        for i in results_pool:
-                            results = results + i
-                        result_data.append((cores, end - start, len(results)))
-                        print("KMP parallel async elapsed: ", end - start)
-                    time.sleep(1)
-            print(result_data)
-            log_data(log_file, result_data)
     else:
-        if naive:
-            
-            for cores in core_count_list:
-                print(f"\nStarting tests with {cores} cores")
-                sliced_text, segments = pp.get_text_segments(text, cores)
+        print("Using KMP search algorithm!")
+        lps = sequentialSM.build_lps(pattern)
+        core_count_list=[4]
+        for cores in core_count_list:
+            print("Starting tests with {} core(s)".format(cores))
+            sliced_text, segments = pp.get_text_segments(text, cores)
 
-                with mp.Pool(cores) as pool:
-                    for iteration in range(repeat): 
-                        start = time.time()
-                        results_pool = pool.starmap(
-                            parallelSM.naive_algorithm, 
-                            [(pattern, x, y) for x, y in zip(sliced_text, segments)]
-                        )
-                        end = time.time()
+            with mp.Pool(cores) as pool:
+                for iteration in range(repeat):
+                    print("    Iteration {}".format(iteration)) 
+                    start = time.time()
+                    results_pool = pool.starmap_async(
+                        parallelSM.kmp_algorithm, 
+                        [(pattern, x, y, lps) for x, y in zip(sliced_text, segments)]
+                    )
+                    results_pool = results_pool.get()
+                    end = time.time()
 
-                        results = list()
-                        for i in results_pool:
-                            results = results + i
-                        result_data.append((cores, end - start, len(results)))
-                        # print("Naive parallel synced elapsed: ", end - start)
-                    time.sleep(1)
-            print(result_data)
-            log_data(log_file, result_data)
+                    results = list()
+                    for i in results_pool:
+                        results = results + i
+                    result_data.append((cores, end - start, len(results)))
+                    print("        KMP parallel elapsed: ", end - start)
+                time.sleep(1)
+        log_data(log_file, result_data)
 
-        else:
-            lps = sequentialSM.build_lps(pattern)
-
-            for cores in core_count_list:
-                print(f"Starting tests with {cores} core(s)")
-                sliced_text, segments = pp.get_text_segments(text, cores)
-
-                with mp.Pool(cores) as pool:
-                    for iteration in range(repeat):
-                        print(f"    Iteration {iteration}") 
-                        start = time.time()
-                        results_pool = pool.starmap(
-                            parallelSM.kmp_algorithm, 
-                            [(pattern, x, y) for x, y in zip(sliced_text, segments)]
-                        )
-                        end = time.time()
-
-                        results = list()
-                        for i in results_pool:
-                            results = results + i
-                        result_data.append((cores, end - start, len(results)))
-                        # print("Naive parallel synced elapsed: ", end - start)
-                    time.sleep(1)
-            print(result_data)
-            log_data(log_file, result_data)
-
-if __name__ == "__main__":
+def main():
+    """
+    Test used for scaling analisys data collection.
+    """
 
     for i in range(250, 2001, 250):
-        #print(f"Creating file with {i} times the size of base sample text!")
         create_large_file("sample_txts/kafka.txt", i)
     
     # Incremets the file size in aproximately 200MB
@@ -161,18 +146,25 @@ if __name__ == "__main__":
                 "kafka_1750.txt",
                 "kafka_2000.txt"
     ]
-    # print(get_core_increment(4))
     log_output_file = "results_log.txt"
     log_data(log_output_file, "RESULTS LOG FILE")
 
     pat = "people"
     for i in files:
-        print(f"Searching in {i} file!")
+        print("Searching in {} file!".format(i))
         current_text = os.path.join(base_path, i)
         loaded_text = load_text(current_text)
-        execute_algorithm(pat, loaded_text, log_output_file, naive=False, exec_async=True)
+        execute_algorithm(pat, loaded_text, log_output_file, naive=False)
         del loaded_text
-        time.sleep(2)
+        time.sleep(1)
+
+if __name__ == "__main__":
+
+    ## This script must be executed from project source 
+    ## (string-matching-parallel directory)
+
+    ## Use python3 scripts/scale_execution.py
+    main()
 
         
  
